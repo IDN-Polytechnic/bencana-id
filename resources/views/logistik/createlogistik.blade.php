179c35db32a9e96b25cc034ffa5dbc7a998516e7
syafiq1331 <bussinesyafiq@gmail.com>
2022-11-27 14:30:15 +0700

    add include function for all file + make it as component

--- resources/views/logistik/createlogistik.blade.php
+++ resources/views/logistik/createlogistik.blade.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
-    @include('template/header')
-</head>
+@include('template/header')
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
